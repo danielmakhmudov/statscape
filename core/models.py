3d7bc066bed1ee56f5658591eb53395302ba3672
Daniel Makhmudov <makhmudovdanil@gmail.com>
2025-05-22 18:51:21 +0200

    Implemented models for user library

--- core/models.py
+++ core/models.py
@@ -36,3 +36,37 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.nickname or f"User {self.steam_id}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    app_id = models.CharField(max_length=65, unique=True)
+    name = models.CharField(max_length=64)
+    genres = models.ManyToManyField(Genre, related_name="games")
+    logo_url = models.URLField(blank=True, null=True)
+    rating = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class UserGame(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    total_playtime = models.IntegerField(default=0)
+    recent_playtime = models.IntegerField(default=0)
+    last_played = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.game}"
+
+    class Meta:
+        unique_together = ("user", "game")
+        ordering = ["-last_played"]
