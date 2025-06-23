520be9a5d6aaa70aae9a23c6da98d6b1fedc9fb2
Daniel Makhmudov <makhmudovdanil@gmail.com>
2025-06-23 19:03:07 +0200

    refactor: extracted users app from core

--- steamscape/settings.py
+++ steamscape/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
     "core",
 ]
 
@@ -130,4 +131,4 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "core.User"
+AUTH_USER_MODEL = "users.User"
