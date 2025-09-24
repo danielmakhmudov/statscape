f834b9029d09dabf043042ae3c0107777cf784fd
Daniel Makhmudov <makhmudovdanil@gmail.com>
2025-09-24 19:22:08 +0200

    feat: implemet basic DashboardView

--- core/views.py
+++ core/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
+from core.services.user_data_service import get_or_fetch_user_profile
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-def index(request):
-    return HttpResponse("")
+class DashboardView(LoginRequiredMixin, TemplateView):
+    login_url = "/login/"
+    redirect_field_name = "next"
+    template_name = "core/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        social = self.request.user.social_auth.get(provider="steam")
+        steam_id = social.uid
+        user_data = get_or_fetch_user_profile(steam_id=steam_id)
+
+        context["user_data"] = user_data
+        return context
