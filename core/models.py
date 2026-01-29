f16afe1d14e6735436b0445e77e0202c99332618
Daniel Makhmudov <makhmudovdanil@gmail.com>
2026-01-29 18:42:18 +0100

    feat: add Theme model, deleted price field from Game model

--- core/models.py
+++ core/models.py
@@ -9,13 +9,20 @@ class Genre(models.Model):
         return self.name
 
 
+class Theme(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Game(models.Model):
     app_id = models.CharField(max_length=65, unique=True)
     name = models.CharField(max_length=64)
     genres = models.ManyToManyField(Genre, related_name="games", blank=True)
+    themes = models.ManyToManyField(Theme, related_name="theme", blank=True)
     logo_url = models.URLField(blank=True, null=True)
-    rating = models.CharField(max_length=100, blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, blank=True, null=True)
 
     def __str__(self):
         return self.name
