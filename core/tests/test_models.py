769289c3a13861105b8186b5e13ab8a96bf02e8e
Daniel Makhmudov <makhmudovdanil@gmail.com>
2026-02-18 17:50:56 +0100

    feat: implement tests for Theme model

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -1,8 +1,15 @@
 import pytest
-from core.models import Genre
+from core.models import Genre, Theme
 
 
 @pytest.mark.django_db
-def test_genre_instance_creation():
+def test_genre_model():
     genre_instance = Genre.objects.create(name="Action")
     assert str(genre_instance) == "Action"
+
+
+@pytest.mark.django_db
+def test_theme_model():
+    theme_instance = Theme.objects.create(igdb_id=100, name="Action")
+    assert str(theme_instance) == "Action"
+    assert theme_instance.igdb_id == 100
