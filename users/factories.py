9a62a199bccba9f24a73c32246b9235a0ebaf3af
Daniel Makhmudov <makhmudovdanil@gmail.com>
2026-02-23 18:06:57 +0100

    refactor: change steam_user_since to past_datetime in UserFactory

--- users/factories.py
+++ users/factories.py
@@ -12,7 +12,7 @@ class UserFactory(DjangoModelFactory):
     nickname = factory.Sequence(lambda n: f"Name_{n}")
     realname = factory.Sequence(lambda n: f"Name_{n} Surname_{n}")
     avatar_url = factory.Sequence(lambda n: f"https://avatar_url-{n}.com")
-    steam_user_since = factory.Faker("date_time_this_decade", tzinfo=datetime.timezone.utc)
+    steam_user_since = factory.Faker("past_datetime", tzinfo=datetime.timezone.utc)
 
     @classmethod
     def _create(cls, model_class, *args, **kwargs):
