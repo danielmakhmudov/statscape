8ad2e52c367aea7d5d12370ca578e1f6daf63088
Daniel Makhmudov <makhmudovdanil@gmail.com>
2026-02-26 17:51:59 +0100

    test: cover with tests get_chart_data function

--- core/tests/test_stats_service.py
+++ core/tests/test_stats_service.py
@@ -1,4 +1,5 @@
-from core.services.stats_service import enrich_games_with_stats
+import pytest
+from core.services.stats_service import enrich_games_with_stats, get_chart_data
 from core.factories import UserGameFactory
 
 
@@ -54,3 +55,45 @@ def test_enrich_games_with_stats_round():
     assert enriched_games[0].playtime_hours == 1.7
     assert enriched_games[0].recent_playtime_hours == 1.7
     assert total_hours == 1.7
+
+
+def test_get_chart_data_basic():
+    user_games = UserGameFactory.build_batch(20, total_playtime=60)
+    enriched_games, total_hours = enrich_games_with_stats(user_games)
+
+    chart_labels, chart_values, chart_hours = get_chart_data(enriched_games)
+
+    assert len(chart_labels) == len(chart_values) == len(chart_hours) == 11
+    assert chart_labels[-1] == "Others"
+    assert sum(chart_values) == pytest.approx(100, abs=0.1)
+    assert sum(chart_hours) == total_hours
+
+
+def test_get_chart_data_only_top_games():
+    user_games = UserGameFactory.build_batch(10)
+    enriched_games, _ = enrich_games_with_stats(user_games)
+
+    chart_labels, chart_values, chart_hours = get_chart_data(enriched_games)
+
+    assert len(chart_labels) == len(chart_values) == len(chart_hours) == 10
+    assert "Others" not in chart_labels
+
+
+def test_get_chart_data_more_than_10_but_others_zero_percent():
+    user_games = UserGameFactory.build_batch(10) + UserGameFactory.build_batch(
+        5, recent_playtime=0, total_playtime=0
+    )
+    enriched_games, _ = enrich_games_with_stats(user_games)
+
+    chart_labels, chart_values, chart_hours = get_chart_data(enriched_games)
+
+    assert len(chart_labels) == len(chart_values) == len(chart_hours) == 10
+    assert "Others" not in chart_labels
+
+
+def test_get_chart_data_empty_list():
+    chart_labels, chart_values, chart_hours = get_chart_data([])
+
+    assert chart_labels == []
+    assert chart_values == []
+    assert chart_hours == []
