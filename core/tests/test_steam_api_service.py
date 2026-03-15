06eab4f6844e8f365450e8d7c1d451e616c0c481
Daniel Makhmudov <makhmudovdanil@gmail.com>
2026-03-15 17:37:47 +0100

    add test for get_user_library function

--- core/tests/test_steam_api_service.py
+++ core/tests/test_steam_api_service.py
@@ -82,3 +82,112 @@ def test_get_user_profile_request_exception(monkeypatch, caplog):
 
     assert response == {}
     assert "Steam API error for user 123:" in caplog.text
+
+
+def test_get_user_library_success(monkeypatch):
+    monkeypatch.setenv("STEAM_API_KEY", "fake-key")
+    steam_api_instance = SteamAPI()
+
+    mock_response = Mock()
+    mock_response.raise_for_status.return_value = None
+    mock_response.json.return_value = {
+        "response": {"game_count": 3, "games": [{"appid": 123}, {"appid": 456}, {"appid": 789}]}
+    }
+    steam_api_instance.session.get = Mock(return_value=mock_response)
+    endpoint = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"
+    params = {
+        "key": "fake-key",
+        "steamid": "123",
+        "include_appinfo": True,
+        "include_played_free_games": True,
+        "format": "json",
+    }
+
+    response = steam_api_instance.get_user_library("123")
+
+    assert response == {"game_count": 3, "games": [{"appid": 123}, {"appid": 456}, {"appid": 789}]}
+    steam_api_instance.session.get.assert_called_once_with(endpoint, params=params)
+
+
+def test_get_user_library_empty_response_dictionary(monkeypatch):
+    monkeypatch.setenv("STEAM_API_KEY", "fake-key")
+    steam_api_instance = SteamAPI()
+    mock_response = Mock()
+    mock_response.raise_for_status.return_value = None
+    mock_response.json.return_value = {"response": {}}
+    steam_api_instance.session.get = Mock(return_value=mock_response)
+
+    response = steam_api_instance.get_user_library(steam_id="123")
+
+    assert response == {}
+    steam_api_instance.session.get.assert_called_once()
+
+
+def test_get_user_library_empty_api_response(monkeypatch):
+    monkeypatch.setenv("STEAM_API_KEY", "fake-key")
+    steam_api_instance = SteamAPI()
+    mock_response = Mock()
+    mock_response.raise_for_status.return_value = None
+    mock_response.json.return_value = {}
+    steam_api_instance.session.get = Mock(return_value=mock_response)
+
+    response = steam_api_instance.get_user_library(steam_id="123")
+
+    assert response == {}
+    steam_api_instance.session.get.assert_called_once()
+
+
+def test_get_user_library_empty_games_list(monkeypatch):
+    monkeypatch.setenv("STEAM_API_KEY", "fake-key")
+    steam_api_instance = SteamAPI()
+    mock_response = Mock()
+    mock_response.raise_for_status.return_value = None
+    mock_response.json.return_value = {"response": {"game_count": 0, "games": []}}
+    steam_api_instance.session.get = Mock(return_value=mock_response)
+
+    response = steam_api_instance.get_user_library(steam_id="123")
+
+    assert response == {"game_count": 0, "games": []}
+    steam_api_instance.session.get.assert_called_once()
+
+
+def test_get_user_library_without_games_list(monkeypatch):
+    monkeypatch.setenv("STEAM_API_KEY", "fake-key")
+    steam_api_instance = SteamAPI()
+    mock_response = Mock()
+    mock_response.raise_for_status.return_value = None
+    mock_response.json.return_value = {"response": {"game_count": 0}}
+    steam_api_instance.session.get = Mock(return_value=mock_response)
+
+    response = steam_api_instance.get_user_library(steam_id="123")
+
+    assert response == {}
+    steam_api_instance.session.get.assert_called_once()
+
+
+def test_get_user_library_raise_for_status_exception(monkeypatch, caplog):
+    monkeypatch.setenv("STEAM_API_KEY", "fake-key")
+    steam_api_instance = SteamAPI()
+    mock_response = Mock()
+    mock_response.raise_for_status.side_effect = requests.RequestException()
+    steam_api_instance.session.get = Mock(return_value=mock_response)
+
+    with caplog.at_level(logging.ERROR):
+        response = steam_api_instance.get_user_library(steam_id="123")
+
+    assert "Steam API error for user 123:" in caplog.text
+    assert response == {}
+    steam_api_instance.session.get.assert_called_once()
+
+
+def test_get_user_library_request_exception(monkeypatch, caplog):
+    monkeypatch.setenv("STEAM_API_KEY", "fake-key")
+    steam_api_instance = SteamAPI()
+    steam_api_instance.session.get = Mock(side_effect=requests.RequestException("network error"))
+
+    with caplog.at_level(logging.ERROR):
+        response = steam_api_instance.get_user_library(steam_id="123")
+
+    assert "Steam API error for user 123:" in caplog.text
+    assert response == {}
+    steam_api_instance.session.get.assert_called_once()
